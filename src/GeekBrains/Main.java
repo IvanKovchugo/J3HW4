916e8d91975b9a39ca3422013a391bf96a9be3f2
kovsi <kovsiun@gmail.com>
2022-02-11 13:24:19 +0100

    Initial commit

---
+++ src/GeekBrains/Main.java
@@ -0,0 +1,10 @@
+package GeekBrains;
+
+public class Main {
+    private final Object mon = new Object();
+    private volatile char letter = 'A';
+
+    public static void main(String[] args) {
+	Main main = new Main()
+    }
+}
